e797af592aaffb2d03ea0bee8b8e19b8f1014eb4
BCA-Simranjeet-Kaur-7238 <126250433+BCA-Simranjeet-Kaur-7238@users.noreply.github.com>
2023-03-18 10:24:07 +0530

    Update Assignment6.c

--- Assignment6.c
+++ Assignment6.c
@@ -1,6 +1,21 @@
 //Write a C code to find the number of distinct elements in a sorted array.
 
-int main()
-{
-  return 0;
+#include <stdio.h>
+int main() {
+    int arr[60],n, distinct_element = 1;
+    printf(" PLEASE ENTER THE NUMBER OF ELEMENTS IN THE ARRAY : ");
+    scanf("%d", &n);
+
+    printf("\n ENTER THE ELEMENTS OF THE ARRAY IN SORTED ORDER HERE :\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[i-1]) {
+            distinct_element++;
+        }
+    }
+    // output
+    printf("\n\tNumber of distinct elements in the array: %d\n", distinct_element);
+    return 0;
 }
