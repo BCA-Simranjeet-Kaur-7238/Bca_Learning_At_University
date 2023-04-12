44d2085ce18d72240db5c6ebafea4751d038bdb9
Simranjeet Kaur <126250433+BCA-Simranjeet-Kaur-7238@users.noreply.github.com>
2023-04-12 21:35:10 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -2,7 +2,27 @@
 //input: Anitha
 //output: Aahint 
 
-int main()
-{
-  return 0;
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+   char string[100], temp;
+    int length, i, j;
+    
+    printf("PLEASE ENTER A STRING : ");
+    scanf("%s", string);
+    
+    length = strlen(string);
+    // Sort in alphabetical order using bubble sort
+    for (i = 0; i < length-1; i++) {
+        for (j = 0; j < length-i-1; j++) {
+            if (string[j] > string[j+1]) {
+                temp = string[j];
+                string[j] = string[j+1];
+                string[j+1] = temp;
+            }
+        }
+    }
+    printf("SORTED STRING IS : %s\n", string);
+    return 0;
 }
