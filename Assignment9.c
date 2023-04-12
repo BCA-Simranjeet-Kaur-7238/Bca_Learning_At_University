ebb13c23e44524b1c84ef1e3709e84c95b95e508
Simranjeet Kaur <126250433+BCA-Simranjeet-Kaur-7238@users.noreply.github.com>
2023-04-12 22:17:58 +0530

    Update Assignment9.c

--- Assignment9.c
+++ Assignment9.c
@@ -5,8 +5,29 @@
 //.       m = 1
 //.       n = 1
 
-int main()
-{
-  
-  return 0;
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[100];
+    int count[100] = {0}; // initialize all elements to 0
+    int i, length;
+    
+    printf("Enter the name: ");
+    scanf("%s", name);
+    length = strlen(name);
+    
+    // to count the frequency of each letter in the name
+    for (i = 0; i < length; i++) {
+        count[name[i] - 'a']++;
+    }
+    
+    // to print the frequency of each letter
+    for (i = 0; i < 26; i++) {
+        if (count[i] > 0) {
+            printf("%c - %d\n", 'a' + i, count[i]);
+        }
+    }
+    return 0;
 }
